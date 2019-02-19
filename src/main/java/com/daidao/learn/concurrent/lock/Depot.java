e90c6f4e28cd13154d2f7e6a3fce692796807f88
KevinYounGa <ouyangjia121@gmail.com>
2019-02-19 15:56:50 +0800

    concurrent lock

---
+++ src/main/java/com/daidao/learn/concurrent/lock/Depot.java
@@ -0,0 +1,90 @@
+package com.daidao.learn.concurrent.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * LockTest1.java
+ * http://www.cnblogs.com/skywang12345/p/3496101.html
+ * ReentrantLock是一个可重入的互斥锁，又被称为“独占锁”。
+ * 顾名思义，ReentrantLock锁在同一个时间点只能被一个线程锁持有；而可重入的意思是，ReentrantLock锁，可以被单个线程多次获取。
+ * ReentrantLock分为“公平锁”和“非公平锁”。它们的区别体现在获取锁的机制上是否公平。“锁”是为了保护竞争资源，
+ * 防止多个线程同时操作线程而出错，ReentrantLock在同一个时间点只能被一个线程获取(当某线程获取到“锁”时，其它线程就必须等待)；
+ * ReentraantLock是通过一个FIFO的等待队列来管理获取该锁所有线程的。在“公平锁”的机制下，线程依次排队获取锁；而“非公平锁”在锁是可获取状态时，
+ * 不管自己是不是在队列的开头都会获取锁。
+ * */
+public class Depot {
+    private int capacity;    // 仓库的容量
+    private int size;        // 仓库的实际数量
+    private Lock lock;        // 独占锁
+    //Condition是需要和Lock联合使用的：通过Condition中的await()方法，能让线程阻塞[类似于wait()]；通过Condition的signal()方法，能让唤醒线程[类似于notify()]。
+    private Condition fullCondtion;            // 生产条件
+    private Condition emptyCondtion;        // 消费条件
+
+    public Depot(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.lock = new ReentrantLock();
+        this.fullCondtion = lock.newCondition();
+        this.emptyCondtion = lock.newCondition();
+    }
+
+    public void produce(int val) {
+        lock.lock();
+        try {
+            // left 表示“想要生产的数量”(有可能生产量太多，需多此生产)
+            int left = val;
+            while (left > 0) {
+                // 库存已满时，等待“消费者”消费产品。
+                while (size >= capacity)
+                    fullCondtion.await();
+                // 获取“实际生产的数量”(即库存中新增的数量)
+                // 如果“库存”+“想要生产的数量”>“总的容量”，则“实际增量”=“总的容量”-“当前容量”。(此时填满仓库)
+                // 否则“实际增量”=“想要生产的数量”
+                int inc = (size+left)>capacity ? (capacity-size) : left;
+                size += inc;
+                left -= inc;
+                System.out.printf("%s produce(%3d) --> left=%3d, inc=%3d, size=%3d\n",
+                        Thread.currentThread().getName(), val, left, inc, size);
+                // 通知“消费者”可以消费了。
+                emptyCondtion.signal();
+            }
+        } catch (InterruptedException e) {
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void consume(int val) {
+        lock.lock();
+        try {
+            // left 表示“客户要消费数量”(有可能消费量太大，库存不够，需多此消费)
+            int left = val;
+            while (left > 0) {
+                // 库存为0时，等待“生产者”生产产品。
+                while (size <= 0)
+                    emptyCondtion.await();
+                // 获取“实际消费的数量”(即库存中实际减少的数量)
+                // 如果“库存”<“客户要消费的数量”，则“实际消费量”=“库存”；
+                // 否则，“实际消费量”=“客户要消费的数量”。
+                int dec = (size<left) ? size : left;
+                size -= dec;
+                left -= dec;
+                System.out.printf("%s consume(%3d) <-- left=%3d, dec=%3d, size=%3d\n",
+                        Thread.currentThread().getName(), val, left, dec, size);
+                fullCondtion.signal();
+            }
+        } catch (InterruptedException e) {
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String toString() {
+        return "capacity:"+capacity+", actual size:"+size;
+    }
+}
+
+
+
